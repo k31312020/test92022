d82a091dd379ceb678e79d99851cd708325604ca
Kuenzang Gyeltshen <kuenzngg@gmail.com>
2022-05-09 16:57:09 +1000

    update part2

--- part2.js
+++ part2.js
@@ -7,12 +7,26 @@ const convertInput = (input) => {
 }
 
 const getPetersLastNumber = (input) => {
-    for(let i = 1; i < input.length; i++) {
-        if (input[i] < input[i-1]) {
-            input[i]--;
-        }
+    for(let i = 0; i < input.length; i++) {
+        reduceNumber(input, i);
     }
     return input;
+}
+
+const reduceNumber = (input, i) => {
+    while (input[i] < input[i-1]) {
+        if (input[i] === 0) {
+            input[i] = 9;
+            for(let j = i-1; i > 0; i--) {
+                if(input[j] > input[j-1]) {
+                    input[j]--;
+                }
+            }
+        } else {
+            input[i]--;
+        }
+        console.log(input)
+    }
 }
 
 console.log(getPetersLastNumber(convertInput(input)))
